695187ab448f107f7b30edfd68e27c35b84b7123
andree-ua <andree.uriarte09@gmail.com>
2025-11-26 13:17:21 -0500

    agregando mas botones

--- JS/primerejemplo.js
+++ JS/primerejemplo.js
@@ -24,9 +24,33 @@ function fondoAside(){
     document.getElementById('aside').style.background='#f584f5ff';
 }
 
-function a1(){ alert("Profe, si esto sale mal… no fui yo "); }
-function a2(){ alert("Ok, creo que ya estoy entendiendo esto "); }
-function a3(){ alert("Uy… le piqué y funcionó "); }
-function a4(){ alert("Profe, esto está más fácil de lo que pensé "); }
-function a5(){ alert("Estoy practicando para que me suba la nota "); }
-function a6(){ alert("Listo profe, otro botón más pa' la colección "); }
+function a1(){ 
+    alert("Profe, si esto sale mal, no fui yo 😭"); 
+}
+function a2(){ 
+    alert("Ok, creo que ya estoy entendiendo esto 😎"); 
+}
+function a3(){ 
+    alert("Uy, le piqué y funcionó "); 
+}
+function a4(){ 
+    alert("Profe, esto está más fácil de lo que pensé "); 
+}
+function a5(){ 
+    alert("Estoy practicando para que me suba la nota "); 
+}
+function a6(){
+    lert("Listo profe, otro botón más pa' la colección "); 
+}
+function fondo(){
+    document.body.style.background = "lightblue";
+}
+function textoColor(){
+    document.body.style.color = "red";
+}
+function tamanoImg(){
+    document.getElementById("carita").style.width = "250px";
+}
+function bordeImg(){
+    document.getElementById("carita").style.border = "2px solid black";
+}

eef743c88f275385b95bd51c82b5468f58ab309f
andree-ua <andree.uriarte09@gmail.com>
2025-11-26 12:05:00 -0500

    corrigiendo errores

--- JS/primerejemplo.js
+++ JS/primerejemplo.js
@@ -2,7 +2,8 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').scr='Imagenes/sad.png';
+    document.getElementById('carita').src='Imagenes/sad.png';
+    alert("Po que ta triste mano");
 }
 function feliz(){
     document.getElementById('carita').src='Imagenes/happy.png';

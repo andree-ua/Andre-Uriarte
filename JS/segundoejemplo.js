d74b0255c51890d9f647f9adb01d925fbf6aecf3
andree-ua <andree.uriarte09@gmail.com>
2025-11-27 13:53:55 -0500

    diseño a la calculadora

--- JS/segundoejemplo.js
+++ JS/segundoejemplo.js
@@ -1,32 +1,27 @@
-document.getElementById("calcular").addEventListener("click", function () {
-    const n1 = parseFloat(document.getElementById("numero1").value);
-    const n2 = parseFloat(document.getElementById("numero2").value);
-    const operacion = document.getElementById("operacion").value;
+document.getElementById("calcular").addEventListener("click", function(){
+    let num1 = parseFloat(document.getElementById("numero1").value);
+    let num2 = parseFloat(document.getElementById("numero2").value);
+    let operacion = document.getElementById("operacion").value;
+    let resultado = document.getElementById("resultado");
 
-    let resultado;
-
-    if (isNaN(n1) || isNaN(n2)) {
-        resultado = "Por favor ingresa números válidos.";
-    } else {
-        switch (operacion) {
-            case "suma":
-                resultado = n1 + n2;
-                break;
-            case "resta":
-                resultado = n1 - n2;
-                break;
-            case "multiplicacion":
-                resultado = n1 * n2;
-                break;
-            case "division":
-                if (n2 === 0) {
-                    resultado = "No se puede dividir entre 0";
-                } else {
-                    resultado = n1 / n2;
-                }
-                break;
-        }
+    if(isNaN(num1)||isNaN(num2)){
+        resultado.textContent = "Ingrese números Valido";
+        return;
     }
-
-    document.getElementById("resultado").textContent = "Resultado: " + resultado;
-});
+function calcular(a,b, op){
+    switch (op) {
+        case "suma":
+            return a+b;     
+        case "resta":
+            return a-b;
+        case "multiplicacion":
+            return a*b;
+        case "division":
+            return a/b;
+        default:
+            return "Operación Inválida"
+    }
+}
+let resultadoFinal = calcular(num1,num2,operacion);
+resultado.textContent="El Resultado es: "+resultadoFinal;
+})
